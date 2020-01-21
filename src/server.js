269ce30bc844ed2e9e14bec11389214eb54843f3
Luqman Olushi <olushilukmon03@gmail.com>
2020-01-21 08:01:10 +0100

    serve static assets

--- src/server.js
+++ src/server.js
@@ -6,6 +6,7 @@ const port = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static('public'))
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
