7da28440e2a9a7c69c4898706672c615b1587819
Luqman Olushi <olushilukmon03@gmail.com>
2020-01-21 08:03:35 +0100

    init firebase

---
+++ public/main.js
@@ -0,0 +1,13 @@
+let firebaseConfig = {
+  apiKey: 'AIzaSyAAv2gk0s_PhEdnxAk2GD00iGPEtDZwvv0',
+  authDomain: 'partner-engagements.firebaseapp.com',
+  databaseURL: 'https://partner-engagements.firebaseio.com',
+  projectId: 'partner-engagements',
+  storageBucket: 'partner-engagements.appspot.com',
+  messagingSenderId: '603090375654',
+  appId: '1:603090375654:web:bcba122f386708adb0bc27',
+  measurementId: 'G-NHQ0BXQXJ2'
+};
+
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
