3e0801c0691da81713c0c141f7bd24c563d623d3
Luqman Olushi <olushilukmon03@gmail.com>
2020-01-21 08:43:23 +0100

    add endpoint to subscribe clients to notification topic

--- src/server.js
+++ src/server.js
@@ -1,13 +1,40 @@
 import express from 'express';
 import bodyParser from 'body-parser';
+import admin from 'firebase-admin';
+
+import serviceAccount from './serviceAccount.json';
 
 const app = express();
 const port = process.env.PORT || 3000;
+const topic = 'partner-engagements';
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount)
+});
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(express.static('public'))
+app.use(express.static('public'));
+
+/**
+ * Gets client (browser) token after they grant notification permission
+ * and subscribes them to a topic for future engagements
+ */
+
+app.post('/api/token/subscribe', (req, res) => {
+  const { token } = req.body;
+
+  if (token) {
+    admin
+      .messaging()
+      .subscribeToTopic(token, topic)
+      .then(() => {
+        res.send({ subscribed: true, topic });
+      })
+      .catch(() => res.status(500).send({ subscribed: false, topic }));
+  }
+});
 
 app.listen(port, () => {
   console.log(`listening on port ${port}`);
-})
+});
